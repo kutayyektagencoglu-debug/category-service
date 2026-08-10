8cdc57faee5fb5a6e0984555c25c270453232f2e
kgencoglu <kutay.gencoglu@migros.com.tr>
2026-08-10 08:54:36 +0300

    added @JsonProperty to dtos

--- src/main/java/com/migros/categoryservice/dto/CategoryResponseDTO.java
+++ src/main/java/com/migros/categoryservice/dto/CategoryResponseDTO.java
@@ -1,5 +1,6 @@
 package com.migros.categoryservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,8 +13,11 @@ import lombok.Setter;
 @Setter
 
 public class CategoryResponseDTO {
+    @JsonProperty("categoryId")
     private Long id;
+    @JsonProperty("categoryName")
     private String name;
     @Size(min = 2, max = 2)
+    @JsonProperty("categoryCode")
     private String code;
 }
